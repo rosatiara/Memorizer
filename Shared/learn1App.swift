cda23a80a77e32ec9cd1e04dd8ab1510976d5f71
tiara <rosatiaragaluh@gmail.com>
2021-11-19 20:58:49 +0700

    Initial Commit

---
+++ Shared/learn1App.swift
@@ -0,0 +1,17 @@
+//
+//  learn1App.swift
+//  Shared
+//
+//  Created by T on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct learn1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
