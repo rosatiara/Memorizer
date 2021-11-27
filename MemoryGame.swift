79afa4f19a08e4f4aad541620bb3f209c0d005a9
rosa <rosatiaragaluh@gmail.com>
2021-11-27 21:05:18 +0700

    modify

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -3,13 +3,20 @@
 //  learn1 (iOS)
 //
 //  Created by T on 27/11/21.
-//
+// This is the Model of our application. All the logics for our game is here.
 
 import Foundation
-struct MemoryGame {
+struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
-    func choose(card: Card) {
+    func choose(_ card: Card) {
         
+    }
+    
+//   MemoryGame.Card
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent // this is a dontcare or generics
     }
 }
