ca1ae23b542f042a4c49dbecf5057f990bb9f918
rosa <rosatiaragaluh@gmail.com>
2021-12-04 19:31:46 +0700

    debug

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -10,7 +10,7 @@ struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
     func choose(_ card: Card) {
-        print("hi gg ...  sdgfdd")
+        print("hello")
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
